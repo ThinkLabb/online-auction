a19ce0cb3daed0f9f588ebf9d77e11e52c61e65a
Hownameee <namhaohuynh@gmail.com>
2026-01-05 14:55:33 +0700

    change env for leapcell setup

--- server/src/config/s3.ts
+++ server/src/config/s3.ts
@@ -2,11 +2,11 @@
 import { S3Client } from '@aws-sdk/client-s3';
 
 export const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.AAWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.AAWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AAWS_SECRET_ACCESS_KEY!,
   },
 });
 
-export const BUCKET_NAME = process.env.AWS_BUCKET_NAME!;
+export const BUCKET_NAME = process.env.AAWS_BUCKET_NAME!;
